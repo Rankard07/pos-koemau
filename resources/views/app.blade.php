54dacb92d4d1b1843c84d85fb6a1dc0bbc10e4b6
Rankard07 <randikadewandaru07@gmail.com>
2026-04-14 07:00:54 +0700

    Fix favicon path by removing /public prefix
    The favicon path has been corrected from `/public/logo/favicon.ico` to `/logo/favicon.ico` to properly serve the favicon file from the web root.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
 
     {{-- <link rel="icon" href="/favicon.ico" sizes="any">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
-    <link rel="icon" href="/public/logo/favicon.ico" sizes="any">
+    <link rel="icon" href="/logo/favicon.ico" sizes="any">
     {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
 
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
